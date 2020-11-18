f44b936489031f48b0479e014fa4886eb75ee02e
Steven <stevenchow33@hotmail.com>
2020-11-18 23:24:04 +0800

    Test

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -8,5 +8,6 @@
 <body>
     <h1>About Us</h1>
     <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, molestias.</h3>
+    <h4>Lorem ipsum dolor sit amet.</h4>
 </body>
 </html>
