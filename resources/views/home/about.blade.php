44ebb8e6d90117eb9a80d6d37bd8c8817dd7d497
Steven <stevenchow33@hotmail.com>
2020-11-18 23:36:42 +0800

    1st newly added

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -9,5 +9,6 @@
     <h1>About Us</h1>
     <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, molestias.</h3>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda odit temporibus veritatis exercitationem reprehenderit maiores sunt totam provident vitae. Voluptatem?</p>
+    <p>Test</p>
 </body>
 </html>
